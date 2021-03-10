4c6bca70db820e54d3cabfc3672dc293c1e4942e
Lenka Benova <dielenka@gmail.com>
2021-03-10 12:28:32 +0100

    Added convolution
    Signed-off-by: Lenka Benova <dielenka@gmail.com>

--- src/Bubbles.cpp
+++ src/Bubbles.cpp
@@ -132,9 +132,82 @@ void CountBubbles()
 	cv::waitKey();
 }
 
+void FindSimilarBubbles()
+{
+	cv::Mat bubble = cv::imread(R"(bubble.png)", 1);
+	cv::Mat src = cv::imread(R"(image_bubbles.jpg)", 1);
+	cv::Mat original;
+	src.copyTo(original);
+
+	cvtColor(bubble, bubble, cv::COLOR_BGR2GRAY);
+	cvtColor(src, src, cv::COLOR_BGR2GRAY);
+
+	GaussianBlur(bubble, bubble, cv::Size(3, 3), 1);
+	GaussianBlur(src, src, cv::Size(3, 3), 1);
+
+	adaptiveThreshold(bubble, bubble, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 35, 0);
+	adaptiveThreshold(src, src, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 35, 0);
+
+	cv::Mat filtered;
+
+	bubble.convertTo(bubble, CV_32F);
+	float bubbleArray[38][36];
+
+	for (int i = 0; i < bubble.rows; ++i)
+	{
+		for (int j = 0; j < bubble.cols; ++j)
+		{
+			if (bubble.at<float>(i, j) == 0)
+				bubbleArray[i][j] = -2;
+			else if (bubble.at<float>(i, j) == 255)
+				bubbleArray[i][j] = 1;
+			else
+				bubbleArray[i][j] = bubble.at<float>(i, j);
+		}
+	}
+
+	cv::Mat kernel = cv::Mat(38, 36, CV_32F, bubbleArray);
+
+	cv::filter2D(src, filtered, CV_8UC1, kernel);
+
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(filtered, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+
+	cvtColor(filtered, filtered, cv::COLOR_GRAY2RGB);
+	int index = 0;
+
+	for (auto i = 0; i < contours.size(); ++i)
+	{
+		drawContours(original, contours, i, cv::Scalar(0, 165, 255), -1);
+
+		double centerX = 0.0;
+		double centerY = 0.0;
+
+		for (const auto& p : contours[i])
+		{
+			centerX += p.x;
+			centerY += p.y;
+		}
+
+		index++;
+		cv::putText(original, std::to_string(index), cv::Point(centerX / contours[i].size(), centerY / contours[i].size()), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 165, 255), 0.5);
+	}
+
+	cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3), cv::Point(1, 1));
+	cv::dilate(filtered, filtered, element);
+
+	imshow("bubble", bubble);
+	imshow("src", src);
+	imshow("Similar", filtered);
+	imshow("final", original);
+
+	cv::waitKey();
+}
+
 int main()
 {
 	CountBubbles();
+	FindSimilarBubbles();
 
     return 0;
 }
